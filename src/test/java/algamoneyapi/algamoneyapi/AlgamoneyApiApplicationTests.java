11bcdb43f8dd122f6d04a65d5ddf2b984fbc642e
Carlos Jesus <carlos.anderson.ti@gmail.com>
2019-11-02 22:47:43 -0300

    Commit initial.

---
+++ src/test/java/algamoneyapi/algamoneyapi/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package algamoneyapi.algamoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
